7abb21008ac098315d500ddffe0d630f21a3ae54
zibixD <Adrianplasek@wp.pl>
2025-02-19 13:20:54 +0100

    created duplicated stars

--- src/app/components/Background/Background.tsx
+++ src/app/components/Background/Background.tsx
@@ -17,20 +17,42 @@ const Background = () => {
 
  useEffect(() => {
   const stars = document.getElementById("stars");
+  const starsDuplicate = document.getElementById("stars-duplicate");
   const stars2 = document.getElementById("stars2");
+  const stars2Duplicate = document.getElementById("stars2-duplicate");
   const stars3 = document.getElementById("stars3");
+  const stars3Duplicate = document.getElementById("stars3-duplicate");
 
-  if (stars && stars2 && stars3) {
-   stars.style.boxShadow = generateShadows(700);
-   stars2.style.boxShadow = generateShadows(200);
-   stars3.style.boxShadow = generateShadows(100);
+  if (
+   stars &&
+   starsDuplicate &&
+   stars2 &&
+   stars2Duplicate &&
+   stars3 &&
+   stars3Duplicate
+  ) {
+   const smallShadows = generateShadows(700);
+   const mediumShadows = generateShadows(200);
+   const bigShadows = generateShadows(100);
+
+   stars.style.boxShadow = smallShadows;
+   starsDuplicate.style.boxShadow = smallShadows;
+
+   stars2.style.boxShadow = mediumShadows;
+   stars2Duplicate.style.boxShadow = mediumShadows;
+
+   stars3.style.boxShadow = bigShadows;
+   stars3Duplicate.style.boxShadow = bigShadows;
   }
  }, []);
  return (
   <>
    <div id="stars"></div>
+   <div id="stars-duplicate"></div>
    <div id="stars2"></div>
+   <div id="stars2-duplicate"></div>
    <div id="stars3"></div>
+   <div id="stars3-duplicate"></div>
   </>
  );
 };
